bd23a2d8b67f534279ba9da878673a7fd81c4b79
ezh-yns <96661138+ezh-yns@users.noreply.github.com>
2024-10-11 21:29:58 +0100

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,4 +4,4 @@ def safe_divide(numerator, denominator):
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:
-        print("Error: Please enter numeric values only.")
+        return "Error: Please enter numeric values only."
